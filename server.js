deac71dcd271b8b549e06c5a960b4a8815fb9c5a
Rowland Momoh <momohrowland@gmail.com>
2026-03-21 00:33:21 +0100

    Add proper error and uncaught exception handling
    - Created errorMiddleware.js with unknownEndpoint and errorHandler functions
    - Updated server.js to use error handling middleware and JSON parsing
    - Added uncaught exception and unhandled rejection handlers for graceful shutdown

--- server.js
+++ server.js
@@ -2,21 +2,41 @@ const express = require('express')
 const cors = require('cors')
 const rootRouter = require('./routes')
 const { connect } = require('./database')
+const {
+  unknownEndpoint,
+  errorHandler,
+} = require('./middlewares/errorMiddleware')
 require('dotenv').config()
+
+/* eslint-disable no-console */
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT EXCEPTION:', err)
+  process.exit(1)
+})
 
 const app = express()
 const port = process.env.PORT || 3030
+let server
 
 app.use(cors())
+app.use(express.json())
 
 app.use('/', rootRouter)
 
-/* eslint-disable no-console */
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 const initServer = async () => {
   await connect()
-  app.listen(port, () => {
+  server = app.listen(port, () => {
     console.log(`Server running on port ${port}`)
   })
 }
+
+process.on('unhandledRejection', err => {
+  console.error('UNHANDLED REJECTION:', err)
+  if (server) server.close(() => process.exit(1))
+  else process.exit(1)
+})
 
 module.exports = { initServer }
